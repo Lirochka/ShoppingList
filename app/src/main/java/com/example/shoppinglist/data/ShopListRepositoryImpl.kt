86be41294aadc9a113f9ce059daa93d97b2f54d2
Lira <lir0na@mail.ru>
2023-07-15 09:24:22 +0300

    Created RecyclerView, swipe  to delete an element, DiffUtil for animation when deleting and installing activity

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,17 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo (p1.id) })
 
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10) {
-         val item = ShopItem("Name$i", i, true)
+        for (i in 0 until 5) {
+         val item = ShopItem("Name$i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
